292061b49391e09f34aab6818082ee7d7845d044
Tzu-Chao, Wang <john29917958@gmail.com>
2018-02-16 20:30:43 +0800

    Completes add shortcut feature

--- swincher/app/components/shortcut/Shortcut.js
+++ swincher/app/components/shortcut/Shortcut.js
@@ -49,6 +49,18 @@ function getInitialState() {
   };
 }
 
+function getUpdatedKeyData(e) {
+  var pressedKeyName = keycode(e.keyCode);
+
+  return {
+    shiftKey: this.state.keyData.shiftKey || e.shiftKey,
+    altKey: this.state.keyData.altKey || e.altKey,
+    ctrlKey: this.state.keyData.ctrlKey || e.ctrlKey,
+    metaKey: this.state.keyData.metaKey || e.metaKey,
+    keyCode: pressedKeyName && !modifierNames.includes(pressedKeyName) ? e.keyCode : this.state.keyData.keyCode
+  };
+}
+
 export default class Shortcut extends Component {
   constructor(props) {
     super(props);
@@ -56,19 +68,15 @@ export default class Shortcut extends Component {
   }
 
   onKeyDown(e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13) { // Enter
       this.props.action(this.state.keyData);
       this.setState(getInitialState.apply(this));
     }
+    else if (e.keyCode === 27) { // Escape
+      this.setState(getInitialState.apply(this));
+    }
     else {
-      var pressedKeyName = keycode(e.keyCode),
-          keyData = {
-            shiftKey: this.state.keyData.shiftKey || e.shiftKey,
-            altKey: this.state.keyData.altKey || e.altKey,
-            ctrlKey: this.state.keyData.ctrlKey || e.ctrlKey,
-            metaKey: this.state.keyData.metaKey || e.metaKey,
-            keyCode: pressedKeyName && !modifierNames.includes(pressedKeyName) ? e.keyCode : this.state.keyData.keyCode
-          },
+      var keyData = getUpdatedKeyData.call(this, e),
           pressedKeys = this.state.pressedKeys;
 
       if (!pressedKeys.includes(e.keyCode)) {
@@ -83,6 +91,13 @@ export default class Shortcut extends Component {
     }
   }
 
+  updateShortcut() {
+    if (!this.props.create) {
+      this.props.action(this.state.keyData);
+      this.setState(getInitialState.apply(this));
+    }
+  }
+
   dummyFunction(e) {
     // do nothing.
   }
@@ -94,7 +109,8 @@ export default class Shortcut extends Component {
         placeholder="Shortcut keys"
         value={this.state.keyNames}
         onKeyDown={this.onKeyDown.bind(this)}
-        onChange={this.dummyFunction} />
+        onChange={this.dummyFunction}
+        onBlur={this.updateShortcut.bind(this)} />
     );
   }
 }
